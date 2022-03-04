ff09d7f5693b0d74066d78a59e82db08a5c707e8
DarkCode <tanzil.ncpsc@gmail.com>
2022-03-04 20:25:24 +0600

    project 7 minor feature

--- js-DOM-Projects/Project 7 - Working with Both Hexadecimal and RGB Color Code/app.js
+++ js-DOM-Projects/Project 7 - Working with Both Hexadecimal and RGB Color Code/app.js
@@ -12,107 +12,108 @@
 // Steps
 
 // Globals
-let div = null;
+let toastMsgDiv = null;
 
 window.onload = () => {
-	main();
+  main();
 };
 
 function main() {
-	const root = document.getElementById('root');
-	const output = document.getElementById('output');
-	const output2 = document.getElementById('output2');
-	const changeBtn = document.getElementById('change-btn');
-	const copyBtn = document.getElementById('copy-btn');
+  const root = document.getElementById("root");
+  const output = document.getElementById("output");
+  const output2 = document.getElementById("output2");
+  const changeBtn = document.getElementById("change-btn");
+  const copyBtn = document.getElementById("copy-btn");
 
-	changeBtn.addEventListener('click', function () {
-		const color = generateColorDecimal();
-		const hex = generateHexColor(color);
-		const rgb = generateRGBColor(color);
-		root.style.backgroundColor = hex;
-		output.value = hex.substring(1);
-		output2.value = rgb;
-	});
+  changeBtn.addEventListener("click", function () {
+    const color = generateColorDecimal();
+    const hex = generateHexColor(color);
+    const rgb = generateRGBColor(color);
+    root.style.backgroundColor = hex;
+    output.value = hex.substring(1);
+    output2.value = rgb;
+  });
 
-	copyBtn.addEventListener('click', function () {
-		navigator.clipboard.writeText(`#${output.value}`);
-		if (div !== null) {
-			div.remove();
-			div = null;
-		}
-		if (isValidHex(output.value)) {
-			generateToastMessage(`#${output.value} copied`);
-		} else {
-			alert('Invalid Color Code');
-		}
-	});
+  copyBtn.addEventListener("click", function () {
+    navigator.clipboard.writeText(`#${output.value}`);
+    if (toastMsgDiv !== null) {
+      removeToastMessage();
+    }
+    if (isValidHex(output.value)) {
+      generateToastMessage(`#${output.value} copied`);
+    } else {
+      alert("Invalid Color Code");
+    }
+  });
 
-	output.addEventListener('keyup', function (e) {
-		const color = e.target.value;
-		if (color) {
-			output.value = color.toUpperCase();
-			if (isValidHex(color)) {
-				root.style.backgroundColor = `#${color}`;
-			}
-		}
-	});
+  output.addEventListener("keyup", function (e) {
+    const color = e.target.value;
+    if (color) {
+      output.value = color.toUpperCase();
+      if (isValidHex(color)) {
+        root.style.backgroundColor = `#${color}`;
+      }
+    }
+  });
 }
 
 // function 1 - generate three random decimal number for red, green and blue
 // return as an object
 function generateColorDecimal() {
-	const red = Math.floor(Math.random() * 255);
-	const green = Math.floor(Math.random() * 255);
-	const blue = Math.floor(Math.random() * 255);
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
 
-	return {
-		red,
-		green,
-		blue,
-	};
+  return {
+    red,
+    green,
+    blue,
+  };
 }
 
 // function 2 - generate hex color code
 function generateHexColor({ red, green, blue }) {
-	const getTwoCode = (value) => {
-		const hex = value.toString(16);
-		return hex.length === 1 ? `0${hex}` : hex;
-	};
+  const getTwoCode = (value) => {
+    const hex = value.toString(16);
+    return hex.length === 1 ? `0${hex}` : hex;
+  };
 
-	return `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(
-		blue
-	)}`.toUpperCase();
+  return `#${getTwoCode(red)}${getTwoCode(green)}${getTwoCode(
+    blue
+  )}`.toUpperCase();
 }
 
 // function 3 - generate rgba color code
 function generateRGBColor({ red, green, blue }) {
-	return `rgb(${red}, ${green}, ${blue})`;
+  return `rgb(${red}, ${green}, ${blue})`;
 }
+function removeToastMessage() {
+  toastMsgDiv.classList.remove("toast-message-slide-in");
+  toastMsgDiv.classList.add("toast-message-slide-out");
 
+  toastMsgDiv.addEventListener("animationend", function () {
+    toastMsgDiv.remove();
+    toastMsgDiv = null;
+  });
+}
 function generateToastMessage(msg) {
-	div = document.createElement('div');
-	div.innerText = msg;
-	div.className = 'toast-message toast-message-slide-in';
+  toastMsgDiv = document.createElement("div");
+  toastMsgDiv.innerText = msg;
+  toastMsgDiv.className = "toast-message toast-message-slide-in";
 
-	div.addEventListener('click', function () {
-		div.classList.remove('toast-message-slide-in');
-		div.classList.add('toast-message-slide-out');
-
-		div.addEventListener('animationend', function () {
-			div.remove();
-			div = null;
-		});
-	});
-
-	document.body.appendChild(div);
+  toastMsgDiv.addEventListener("click", removeToastMessage);
+  document.body.appendChild(toastMsgDiv);
+  setTimeout(() => {
+    removeToastMessage();
+  }, 2000);
 }
 
 /**
  * @param {string} color : ;
  */
 function isValidHex(color) {
-	if (color.length !== 6) return false;
-	return /^[0-9A-Fa-f]{6}$/i.test(color);
+  if (color.length !== 6) return false;
+  return /^[0-9A-Fa-f]{6}$/i.test(color);
 }
 
 // Step 1 - create onload handler
